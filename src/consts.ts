bce0bc563609e78671a53402f577e42ad8a19fcf
= <=>
2023-12-14 17:43:30 +0530

    chore: changed server url

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
 export const introMessage = "Hi, welcome to medical chat. How can I help you?"
-export const SERVER_URL = "http://localhost:4000"
+export const SERVER_URL = "https://medi-assist-backend.vercel.app/"
