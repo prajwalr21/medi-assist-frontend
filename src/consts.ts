8caf1674ebbc8abb6eb76510abcde5116ea2bb39
= <=>
2023-12-14 17:46:38 +0530

    chore: minor changes

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
 export const introMessage = "Hi, welcome to medical chat. How can I help you?"
-export const SERVER_URL = "https://medi-assist-backend.vercel.app/"
+export const SERVER_URL = "https://medi-assist-backend.vercel.app"
