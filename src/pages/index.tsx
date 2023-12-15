8ac132bd2babcbc757e3456c8dac3fde3da89579
= <=>
2023-12-15 12:15:44 +0530

    chore: add env

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import { ChatContainer } from '../components/ChatContainer'
 import { UserInput } from '../components/UserInput'
 import { introMessage } from '../consts'
